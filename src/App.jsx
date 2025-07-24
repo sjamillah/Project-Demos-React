ddffde975f3df8ec5e6ff8f18d3a964320b50e63
sjamillah <j.ssozi@alustudent.com>
2025-07-24 13:22:51 +0200

    the calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Counter from './Counter-App/components/Counter'
 import Todo from './Todo-App/components/Todo'
 import Meals from './Meals-App/components/Meals'
+import Calculator from './Calculator-App/components/Calculator'
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <Todo /> */}
-      <Meals />
+      {/* <Meals /> */}
+      <Calculator />
     </div>
   )
 }
