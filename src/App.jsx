797a991a06487fd87cb8878b32bac6095738fafa
sjamillah <j.ssozi@alustudent.com>
2025-07-24 12:48:34 +0200

    the meal app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Counter from './Counter-App/components/Counter'
 import Todo from './Todo-App/components/Todo'
+import Meals from './Meals-App/components/Meals'
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
-      <Todo />
+      {/* <Todo /> */}
+      <Meals />
     </div>
   )
 }
---
+++ src/Meals-App/components/Meals.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import '../style.css'
+
+const Meals = () => {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        // eslint-disable-next-line no-undef
+        axios
+        .get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+            .then(res => {
+                // console.log(res.data.meals)
+                setItems(res.data.meals)
+            }).catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+    const itemsList = items.map(({strMeal, strMealThumb, idMeal}) => {
+        return (
+            <section className='content' key={idMeal}>
+                <img src={strMealThumb} alt={strMeal} />
+                <p>{strMeal}</p>
+                <p>#{idMeal}</p>
+            </section>
+        )
+    })
+  return (
+    <div className='items-container'>
+      {itemsList}
+    </div>
+  )
+}
+
+export default Meals
